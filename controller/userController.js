95605b401729bda5c8199510f89e51bd9e43a945
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-07-28 19:36:20 +0600

    user data fetch from db

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModels");
 
+// -------- Get all users--------------
+
 exports.getAllUser = async (req, res) =>{
     try {
         const users = await User.find()
@@ -10,3 +12,17 @@ exports.getAllUser = async (req, res) =>{
     }
 }
 
+// ------- Get User --------------
+
+exports.getUser = async (req, res ) => {
+    const userId = req.params.userId;
+    try {
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(400).json("Wrong user")
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        return res.status(401).json({message: error.message})
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
-const { getAllUser } = require("../controller/userController")
+const { getAllUser, getUser } = require("../controller/userController")
 
 const userRoute = require("express").Router()
 
 userRoute.get("/", getAllUser)
+userRoute.get("/:userId", getUser)
 
 module.exports = userRoute;
